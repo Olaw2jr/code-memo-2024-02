afcb5067cba2fac7b71ac61a7555470abfca8890
大城亮 <hiltoc.katsu@gmail.com>
2024-02-21 20:33:08 +0900

    setTimeout関数例

---
+++ 21.js
@@ -0,0 +1,8 @@
+//setTime関数
+function sayHello() {
+    console.log("Hello, World!");
+  }
+  
+  setTimeout(sayHello, 3000); // 3秒後にsayHello()を実行
+  //setTimeout関数は、最初の引数として渡された関数を、2番目の引数として渡されたミリ秒後に実行します。この場合、sayHello関数は3秒後に実行されます
+//画像を自動的に切り替えるといった機能で活用可能
